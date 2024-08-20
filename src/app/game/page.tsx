a5158862d5f03cbdd68a8483459c9e3af42c2fbd
Vebjorn Elvekrok <vebjorn.elvekrok@gmail.com>
2024-08-20 15:27:05 +0200

    Move title to layout, add sign out button

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,25 +1,14 @@
 import { Suspense } from "react";
 import { Board } from "~/app/_components/board";
-import Image from "next/image";
+import { HydrateClient } from "~/trpc/server";
+import { auth } from "~/auth";
 
 export default async function Game() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <main className="flex min-h-screen flex-col items-center bg-[#ddcfaf]">
-        <div className="container flex flex-col items-center justify-center gap-3">
-          <h1 className="relative">
-            <Image
-              src="/banner.webp"
-              alt="Hippieløp title"
-              width={500}
-              height={150}
-            />
-            <div className="absolute inset-0 top-16 bg-gradient-to-t from-[#ddcfaf]" />
-          </h1>
-
-          <Board />
-        </div>
-      </main>
-    </Suspense>
+    <HydrateClient>
+      <Suspense>
+        <Board />
+      </Suspense>
+    </HydrateClient>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { api, HydrateClient } from "~/trpc/server";
-import Image from "next/image";
 import { Suspense } from "react";
 import { Countdown } from "~/app/_components/countdown";
 import { boardOpensDate } from "~/shared/config";
@@ -10,23 +9,9 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center bg-[#ddcfaf]">
-        <div className="container flex flex-col items-center justify-center gap-3">
-          <h1 className="relative">
-            <Image
-              src="/banner.webp"
-              alt="Hippieløp title"
-              width={500}
-              height={150}
-            />
-            <div className="absolute inset-0 top-16 bg-gradient-to-t from-[#ddcfaf]" />
-          </h1>
-
-          <Suspense>
-            <Countdown date={boardOpensDate} doneNode={<LoginForm />} />
-          </Suspense>
-        </div>
-      </main>
+      <Suspense>
+        <Countdown date={boardOpensDate} doneNode={<LoginForm />} />
+      </Suspense>
     </HydrateClient>
   );
 }
