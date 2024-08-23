3b335f805954b00445ef38adec025618f1f5864d
Eik Hvattum Røgeberg <eikhr@stud.ntnu.no>
2024-08-23 21:48:41 +0200

    Fix bug with button group selected colors

--- src/app/_components/button-group.tsx
+++ src/app/_components/button-group.tsx
@@ -28,7 +28,7 @@ export const ButtonGroup = <T extends string>({
           key={item.value}
           value={item.value}
           disabled={item.disabled}
-          className={`border-r-2 bg-primary p-2 text-primary-foreground transition-colors last:border-r-0 hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${value === item.value ? `bg-amber-500 hover:bg-amber-500/90 ${item.selectedClassName}` : ""}`}
+          className={`border-r-2 p-2 text-primary-foreground transition-colors last:border-r-0 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${value === item.value ? (item.selectedClassName ?? "bg-amber-500 hover:bg-amber-500/90") : "bg-primary hover:bg-primary/90"}`}
         >
           {item.label}
         </ToggleGroup.Item>
