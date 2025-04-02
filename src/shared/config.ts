a18b04ddde061e7654105014d6dfbcb705eba08e
Ingeborg Lundamo <112323876+Lundis1@users.noreply.github.com>
2025-04-02 21:18:30 +0200

    change the time

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,2 +1,2 @@
-export const boardOpensDate = new Date("2024-08-24T16:00:00Z");
-export const boardClosesDate = new Date("2024-08-24T19:00:00Z");
+export const boardOpensDate = new Date("2025-04-01T16:00:00Z");
+export const boardClosesDate = new Date("2025-05-01T19:00:00Z");
