acf71b25376e15f347a7d3047ca9219c03365c51
Vebjorn Elvekrok <vebjorn.elvekrok@gmail.com>
2025-08-23 14:38:05 +0200

    Update time

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,2 +1,2 @@
-export const boardOpensDate = new Date("2025-08-23T19:00:00Z");
-export const boardClosesDate = new Date("2025-08-23T23:00:00Z");
+export const boardOpensDate = new Date("2025-08-23T15:00:00Z");
+export const boardClosesDate = new Date("2025-08-23T19:00:00Z");
